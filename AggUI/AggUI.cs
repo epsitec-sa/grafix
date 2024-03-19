5752b4107d6c55e153a4f3959ad8e5daa63b3e14
Baptiste Lambert <lambert@epsitec.ch>
2024-03-19 16:18:03 +0100

    Add SystemTools namespace

--- AggUI/AggUI.cs
+++ AggUI/AggUI.cs
@@ -2,27 +2,29 @@ using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-public class ScreenInfo {
-    private const string LibAgg = "AntigrainCPP";
-	
-	[DllImport(LibAgg, CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
-    private static extern void GetScreenResolution(out int width, out int height);
-	
-	public static Rect GetResolution() {
-		int width;
-		int height;
-		GetScreenResolution(out width, out height);
-		return new Rect(width, height);
-	}
-	
-	public struct Rect {
-		public Rect(int width, int height){
-			this.Width = width;
-			this.Height = height;
+namespace SystemTools {
+	public class ScreenInfo {
+		private const string LibAgg = "AntigrainCPP";
+		
+		[DllImport(LibAgg, CallingConvention = CallingConvention.Cdecl, SetLastError = true)]
+		private static extern void GetScreenResolution(out int width, out int height);
+		
+		public static Rect GetResolution() {
+			int width;
+			int height;
+			GetScreenResolution(out width, out height);
+			return new Rect(width, height);
 		}
 		
-		public int Width {get; set;}
-		public int Height {get; set;}
+		public struct Rect {
+			public Rect(int width, int height){
+				this.Width = width;
+				this.Height = height;
+			}
+			
+			public int Width {get; set;}
+			public int Height {get; set;}
+		}
 	}
 }
 
--- AggUIExample/Main.cs
+++ AggUIExample/Main.cs
@@ -1,4 +1,5 @@
 using AggUI;
+using SystemTools;
 
 namespace Example {
 
