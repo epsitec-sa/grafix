d6a5d07e417675f0e9183cab003ae681414faf26
Baptiste Lambert <lambert@epsitec.ch>
2024-07-22 08:51:51 +0200

    Add license header

--- AggUI/Rasterizer.cs
+++ AggUI/Rasterizer.cs
@@ -1,5 +1,21 @@
-﻿// Copyright © 2003-2024, EPSITEC SA, CH-1400 Yverdon-les-Bains, Switzerland
-// Author: Pierre ARNAUD, Roger VUISTINER, Maintainer: Roger VUISTINER
+﻿/*
+This file is part of AggUI.
+
+Copyright © 2003-2024, EPSITEC SA, 1400 Yverdon-les-Bains, Switzerland
+
+AggUI is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+AggUI is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 using static AntigrainSharp.Native;
 
