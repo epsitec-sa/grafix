045edfa863d7ba91e56b751b50e37093bc72f2fe
Baptiste Lambert <lambert@epsitec.ch>
2024-07-22 09:05:58 +0200

    Add license header

--- AggUI/aggcpp/font.h
+++ AggUI/aggcpp/font.h
@@ -1,3 +1,22 @@
+/*
+This file is part of AggUI.
+
+Copyright © 2003-2024, EPSITEC SA, 1400 Yverdon-les-Bains, Switzerland
+
+AggUI is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+AggUI is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
 #include "agg_conv_contour.h"
